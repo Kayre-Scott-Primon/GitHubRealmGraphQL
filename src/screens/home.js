7147c83d1ee16237139c88fbe9a99494481d4944
KayreScottPrimon <kayre@sensix.com.br>
2021-10-11 09:08:19 -0300

    atualizando home

--- src/screens/home.js
+++ src/screens/home.js
@@ -12,7 +12,7 @@ const QUERY = gql`
           viewer {
             login
             id
-            repositories(last: 5) {
+            repositories(last: 1) {
                nodes {
                  name
                  id
@@ -22,7 +22,31 @@ const QUERY = gql`
         }
      `
 
-     //deve-se ter apenas uma query, ou seja, tem que se buscar todos os arquivos que se queira apenas com uma query
+     const Repo = {
+          name: 'repository',
+          properties: {
+               name: {
+                    type: 'string'
+               },
+               id: {
+                    type: 'string'
+               }
+          }
+     }
+
+     const Schema = {
+          name: "GitHub",
+          properties: {
+            id: "string",
+            user: "string",
+            repo: {
+               type: 'list',
+               objectType: 'repository'
+            }
+          }
+     }
+
+     // deve-se ter apenas uma query, ou seja, tem que se buscar todos os arquivos que se queira apenas com uma query
 
      // obrigatorio ter o loading, se tentar aceesar o objeto antes de finalizar ele crasha
 
@@ -31,6 +55,81 @@ const QUERY = gql`
 export default function ScreenHome() {
 
      const { data, loading } = useQuery(QUERY)
+
+     const saveRealm = async (data) => {
+          const realm = await Realm.open({
+               path: "github",
+               schema: [Schema, Repo],
+          });
+
+          //lendo ...
+          const readRealm = await realm.objects("GitHub");
+          console.log('leitura: ' + readRealm)
+
+          //escrevendo...
+          await realm.write(async () => {
+               const obj = await realm.create("GitHub", {
+                    id: data.viewer.id,
+                    user: data.viewer.login,
+                    repo: [
+                         {name: data.viewer.repositories.nodes[0].name, id: data.viewer.repositories.nodes[0].id}
+                    ]
+               });
+               console.log(`created: ${obj}`);
+          })
+     }
+
+     const readRealm = async () => {
+          const realm = await Realm.open({
+               path: "github",
+               schema: [Schema, Repo],
+          });
+          //lendo ...
+          const readRealm = await realm.objects("GitHub");
+          console.log('leitura: ' + JSON.stringify(readRealm[0]))
+     }
+
+     const deleteRealm = async () => {
+          const realm = await Realm.open({
+               path: "github",
+               schema: [Schema, Repo],
+          });
+          // excluindo ...
+          realm.write(() => realm.deleteAll())
+          console.log('Deletados!')
+     }
+
+     const updateRealm = async (data) => {
+          const realm = await Realm.open({
+               path: "github",
+               schema: [Schema, Repo],
+          });
+
+          //lendo antes...
+          const readRealmB = await realm.objects("GitHub");
+          console.log('leitura antes: ' + JSON.stringify(readRealmB))
+
+          //atualizando...
+          await realm.write(async () => {
+               var obj = realm.objects('GitHub').filtered('id = "' + data.viewer.id + '"')
+               console.log('obj: ' + JSON.stringify(obj[0]))
+               obj[0].repo[0].name = data.viewer.repositories.nodes[0].name 
+               console.log(`updated: ${JSON.stringify(obj[0])}`);
+          })
+
+          //lendo depois ...
+          const readRealmA = await realm.objects("GitHub");
+          console.log('leitura depois: ' + JSON.stringify(readRealmA))
+     }
+
+     useEffect(() => {
+          if(!loading){
+               //saveRealm(data)
+               readRealm()
+               //deleteRealm()
+               updateRealm(data)
+          }
+     },[loading])
 
      useEffect(() => {
           console.log('data ' + data, loading)
